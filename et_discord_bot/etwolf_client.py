4fcc9be38dbc0047d563a824aab9a7c1a152fc08
John Miller <personalcomputer@users.noreply.github.com>
2018-06-19 02:02:18 -0700

    fix congestion control glitches and add retry logic

--- et_discord_bot/etwolf_client.py
+++ et_discord_bot/etwolf_client.py
@@ -1,3 +1,4 @@
+import asyncio_extras
 import asyncio
 import collections
 import datetime
@@ -11,9 +12,9 @@ from async_timeout import timeout
 
 from .util import split_chunks
 
-
-OUTBOUND_GLOBAL_MAX_RATE = 256 * 1024  # Bytes per second
-ET_SERVER_RESPONSE_TIMEOUT = datetime.timedelta(seconds=6)
+OUTBOUND_GLOBAL_MAX_THROUGHPUT = 256*1024   # Bytes per second
+OUTBOUND_GLOBAL_MAX_PACKET_RATE = 500      # Datagrams per second
+ET_SERVER_RESPONSE_TIMEOUT = datetime.timedelta(seconds=5)
 
 
 class ETClientProtocol(asyncio.DatagramProtocol):
@@ -40,9 +41,12 @@ class ETClientProtocol(asyncio.DatagramProtocol):
         # et_discord_bot.
         if ETClientProtocol.last_sent_message_timestamp:
             wait = None
-            while wait > 0 or wait is None:
-                interval = OUTBOUND_GLOBAL_MAX_RATE / ETClientProtocol.last_sent_message_length
-                wait = self.loop.time() - (ETClientProtocol.last_sent_message_timestamp + interval)
+            while wait is None or wait > 0:
+                interval = max(
+                    ETClientProtocol.last_sent_message_length / OUTBOUND_GLOBAL_MAX_THROUGHPUT,
+                    1/OUTBOUND_GLOBAL_MAX_PACKET_RATE
+                )
+                wait = (ETClientProtocol.last_sent_message_timestamp + interval) - self.loop.time()
                 if wait > 0:
                     await asyncio.sleep(wait)
 
@@ -125,9 +129,13 @@ class ETClientProtocol(asyncio.DatagramProtocol):
 
     def error_received(self, exc):
         logging.error(f'ETClientProtocol: Error received: {str(exc)}')
+        if self._waiter is not None:
+            self._waiter.set_result(None)
 
     def connection_lost(self, exc):
         logging.debug('ETClientProtocol: Socket closed')
+        if self._waiter is not None:
+            self._waiter.set_result(None)
 
 
 class ETClient(object):
@@ -138,49 +146,56 @@ class ETClient(object):
     def __init__(self, loop=None):
         self.loop = loop or asyncio.get_event_loop()
 
+    @asyncio_extras.async_contextmanager
+    async def connect(self, addr):
+        transport, protocol = await self.loop.create_datagram_endpoint(
+            lambda: ETClientProtocol(self.loop),
+            remote_addr=addr
+        )
+        try:
+            yield protocol
+        finally:
+            transport.close()
+
     async def get_server_list(self, master_server_addr=None):
         if master_server_addr is None:
             master_server_addr = (socket.gethostbyname(ETClient.MASTER_SERVER_HOST), ETClient.MASTER_SERVER_PORT)
 
-        transport, protocol = await self.loop.create_datagram_endpoint(
-            lambda: ETClientProtocol(self.loop),
-            remote_addr=master_server_addr
-        )
-        try:
-            await protocol.send_getservers()
+        async with self.connect(master_server_addr) as protocol:
+            try:
+                await protocol.send_getservers()
 
-            while True:
-                async with timeout(ET_SERVER_RESPONSE_TIMEOUT.total_seconds()):
-                    await protocol.wait_for_message()
-        except asyncio.TimeoutError:
-            if not protocol.message_queue:
-                raise
-            else:
-                pass
-        finally:
-            transport.close()
+                while True:
+                    async with timeout(ET_SERVER_RESPONSE_TIMEOUT.total_seconds()):
+                        await protocol.wait_for_message()
+            except asyncio.TimeoutError:
+                if not protocol.message_queue:
+                    raise
+                else:
+                    pass
 
-        servers = []
-        for message_type, servers_part in protocol.message_queue:
-            if message_type != 'getserversResponse':
-                raise ValueError()
-            servers.extend(servers_part)
+            servers = []
+            for message_type, servers_part in protocol.message_queue:
+                if message_type != 'getserversResponse':
+                    raise ValueError()
+                servers.extend(servers_part)
 
-        return servers
+            return servers
 
     async def get_server_info(self, server, port):
-        transport, protocol = await self.loop.create_datagram_endpoint(
-            lambda: ETClientProtocol(self.loop),
-            remote_addr=(server, port)
-        )
-        try:
-            await protocol.send_getinfo()
+        async with self.connect((server, port)) as protocol:
+            tries = 3
+            while tries > 0:
+                await protocol.send_getinfo()
 
-            async with timeout(ET_SERVER_RESPONSE_TIMEOUT.total_seconds()):
-                await protocol.wait_for_message()
-                message_type, message_content = protocol.message_queue.pop()
-                if message_type != 'infoResponse':
-                    raise ValueError()
-                return message_content
-        finally:
-            transport.close()
+                try:
+                    async with timeout(ET_SERVER_RESPONSE_TIMEOUT.total_seconds()):
+                        await protocol.wait_for_message()
+                        message_type, message_content = protocol.message_queue.pop()
+                        if message_type != 'infoResponse':
+                            raise ValueError()
+                        return message_content
+                except asyncio.TimeoutError:
+                    tries -= 1
+                    if not tries:
+                        raise
