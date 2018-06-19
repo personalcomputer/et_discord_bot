eaed5f06dd7bf3442e0df56450e75ca9990c0532
John Miller <personalcomputer@users.noreply.github.com>
2018-06-19 01:20:47 -0700

    add health check and termination on unhealthy

--- et_discord_bot/__main__.py
+++ et_discord_bot/__main__.py
@@ -5,13 +5,20 @@ from .bot import ETBot
 from .config import config
 
 
+async def terminate_loop_if_unhealthy(loop, health_check):
+    while health_check():
+        await asyncio.sleep(1)
+    loop.stop()
+
+
 def main():
     logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', datefmt='%FT%TZ', level=logging.INFO)
     loop = asyncio.get_event_loop()
 
-    bot = ETBot(config.discord_api_auth_token, loop)
-    loop.create_task(bot.start())
     try:
+        bot = ETBot(config.discord_api_auth_token, loop)
+        loop.create_task(bot.start())
+        loop.create_task(terminate_loop_if_unhealthy(loop, lambda: bot.is_healthy()))
         loop.run_forever()
     except KeyboardInterrupt:
         logout_task = loop.create_task(bot.logout())
